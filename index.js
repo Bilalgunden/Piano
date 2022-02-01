cc4a16d909769fdbfff32dfa426a6e8d3a28de58
Bilalgunden <35685913+Bilalgunden@users.noreply.github.com>
2022-02-01 23:17:37 +0300

    Piano CR Fix
    Record yapısı düzeltildi
    Record alırken play'e basılma hatası çözüldü.
    Switch case yapısı kaldırıldı.
    Source isimleri minimize edildi
    record array'inin boşaltılması düzeltildi.
    Kod vertical alignment'a göre düzenlendi.

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var playButton = document.getElementById('PlayButton');
 var isRecording = false;
 var source = '';
 var recordArray = [];
+var eventTimes = [];
 
-function playSound() {
-    var sound = new Audio(source);
-    sound.play();
-}
+
+container.addEventListener('click',function(elem){
+    source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key' + elem.target.innerText + '.mp3';
+    var eventTime = new Date().getTime();
+    eventTimes.push(eventTime);
+    checkRecord();
+    playSound();
+})
 
 function checkRecord(){
     if(isRecording === true){
@@ -17,90 +22,38 @@ function checkRecord(){
     }
 }
 
-container.addEventListener('click',function(elem){
-    switch(elem.target.innerText){
-        case '1':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key02.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '2':
-            source = 'https://ia800106.us.archive.org/13/items/24-piano-keys/key04.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '3':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key06.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '4':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key08.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '5':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key10.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '6':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key12.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '7':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key14.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '8':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key16.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '9':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key18.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '10':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key20.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '11':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key22.mp3';
-            checkRecord();
-            playSound();
-            break;
-        case '12':
-            source = 'https://ia600106.us.archive.org/13/items/24-piano-keys/key24.mp3';
-            checkRecord();
-            playSound();
-            break;
-        default:
-            console.log('error');
-    }
-})
+function playSound() {
+    var sound = new Audio(source);
+    sound.play();
+}
 
 recordButton.addEventListener('click', function(){
-    recordArray = [];
     isRecording = !isRecording;
     if (isRecording === true){
         recordButton.classList.add("changeColor");
+        recordArray = [];
     }else{
         recordButton.classList.remove('changeColor');
     }
 })
 
+
 playButton.addEventListener('click', function(){
+    var timeoutRange = 0;
+    var timeout = 0;
+    var oldDateTime = 0;
     var sound = new Audio(recordArray[0]);
     sound.play();
     for (let index = 1; index < recordArray.length; index++) {
+        timeout = eventTimes[index] - eventTimes[index-1];
+        timeoutRange = timeout + oldDateTime;
+        oldDateTime = timeoutRange;
         setTimeout(function(){
             var sound2 = new Audio(recordArray[index]);
             sound2.play();
-        },index*1000);
+        },timeoutRange);
     }
+    isRecording = false;
+    recordButton.classList.remove('changeColor');
+    eventTimes = [];
 })
